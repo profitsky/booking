fddc13ed56605441159c15848ef90baaecac2117
piotr <peter.zyskowski@gmail.com>
2021-08-24 17:13:24 +0200

    axios update

--- src/axios-auth.js
+++ src/axios-auth.js
@@ -3,8 +3,8 @@ import axios from 'axios';
 const instance = axios.create({
   baseURL: 'https://identitytoolkit.googleapis.com/v1',
   params: {
-    key: 'AIzaSyDsb92h-vVP2-dzCMdF864HrPXoU-CKLD4' 
-  }
+    key: 'XXX',
+  },
 });
 
 export default instance;
